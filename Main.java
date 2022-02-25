62fba20a5b88a644db9960270cec4c9c3ee76000
RobertsOta <roberts.otankis@gmail.com>
2022-02-25 11:05:31 +0200

    Name change

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("To do list");
+        JFrame frame = new JFrame("To do list panel");
         frame.setContentPane(new Blackberry().panel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
